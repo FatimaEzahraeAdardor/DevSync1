d7a38ea71cc23dbdefefb20bc5d4c8fa259d4c94
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-10-03 16:29:56 +0100

    create class user and enum role

---
+++ src/main/java/org/example/devsync1/entities/User.java
@@ -0,0 +1,38 @@
+package org.example.devsync1.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.example.devsync1.enums.Role;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public User(String username, String password, String firstName, String lastName, String email, Role role) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User() {
+
+    }
+}
---
+++ src/main/java/org/example/devsync1/enums/Role.java
@@ -0,0 +1,6 @@
+package org.example.devsync1.enums;
+
+public enum Role {
+    MANAGER,
+    USER
+}
