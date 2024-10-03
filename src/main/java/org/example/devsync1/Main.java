e058818567a2f6f03547919c51a3b7a3650452ea
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-10-03 16:29:14 +0100

    database connected

---
+++ src/main/java/org/example/devsync1/Main.java
@@ -0,0 +1,17 @@
+package org.example.devsync1;
+
+public class Main {
+    public static void main(String[] args) {
+//        UserRepository userRepository = new UserRepository();
+//        User user = new User();
+//        user.setUsername("admin");
+//        user.setFirstName("John");
+//        user.setLastName("Doe");
+//        user.setEmail("john.doe@example.com");
+//        user.setPassword("password");
+//        user.setRole(Role.USER);
+//        userRepository.save(user);
+//        user.setUsername("user");
+//        userRepository.update(user);
+    }
+}
